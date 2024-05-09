226180504cceabc631eca05f223cacaea90e03d6
Chris Lee <lee.christopher@northeastern.edu>
2024-05-09 17:03:57 -0500

    Create kanbas react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from './logo.svg'
+import Labs from "./Labs";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to Web Dev</h1>
+      <Labs />
+    </div>
+  );
+}

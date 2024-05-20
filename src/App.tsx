b49020083722a619d941db931683243b36557bdf
Chris Lee <lee.christopher@northeastern.edu>
2024-05-20 18:44:03 -0500

    Created dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import logo from './logo.svg'
 import Labs from "./Labs";
+import Kanbas from "./Kanbas"
+import Dashboard from "./Dashboard";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
 export default function App() {
-  return (
-    <div>
-      <h1>Welcome to Web Dev</h1>
-      <Labs />
-    </div>
-  );
+	return (
+			<div id="wd-kanbas">
+				<Dashboard />
+			</div>
+	);
 }
---
+++ src/Dashboard/index.tsx
@@ -0,0 +1,100 @@
+export default function Dashboard() {
+	return (
+		<div id="wd-dashboard">
+			<h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+			<h2 id="wd-dashboard-published">Published Courses (7)</h2> <hr />
+			<div id="wd-dashboard-courses">
+				<div className="wd-dashboard-course">
+					<img src="/images/5610tn.jpg" width={200} />
+					<div>
+						<a className="wd-dashboard-course-link"
+							href="#/Kanbas/Courses/5610/Home">
+							CS5610 Web Development
+						</a>
+						<p className="wd-dashboard-course-title">
+							Web Development
+						</p>
+						<a href="#/Kanbas/Courses/5610/Home"> Go </a>
+					</div>
+				</div>
+				<div className="wd-dashboard-course">
+					<img src="/images/5100tn.jpg" width={200} />
+					<div>
+						<a className="wd-dashboard-course-link"
+							href="#/Kanbas/Courses/5100/Home">
+							CS100 Artificial Intelligence
+						</a>
+						<p className="wd-dashboard-course-title">
+							Foundations of Artificial Intelligence
+						</p>
+						<a href="#/Kanbas/Courses/5100/Home"> Go </a>
+					</div>
+				</div>
+				<div className="wd-dashboard-course">
+					<img src="/images/5800tn.jpg" width={200} />
+					<div>
+						<a className="wd-dashboard-course-link"
+							href="#/Kanbas/Courses/5800/Home">
+							CS5800 Algorithms
+						</a>
+						<p className="wd-dashboard-course-title">
+							Algorithms
+						</p>
+						<a href="#/Kanbas/Courses/5800/Home"> Go </a>
+					</div>
+				</div>
+				<div className="wd-dashboard-course">
+					<img src="/images/1234tn.jpg" width={200} />
+					<div>
+						<a className="wd-dashboard-course-link"
+							href="#/Kanbas/Courses/1234/Home">
+							CS1234 Computer Graphics
+						</a>
+						<p className="wd-dashboard-course-title">
+						Computer Graphics
+						</p>
+						<a href="#/Kanbas/Courses/1234/Home"> Go </a>
+					</div>
+				</div>
+				<div className="wd-dashboard-course">
+					<img src="/images/1235tn.jpg" width={200} />
+					<div>
+						<a className="wd-dashboard-course-link"
+							href="#/Kanbas/Courses/1235/Home">
+							CS1235 Networking
+						</a>
+						<p className="wd-dashboard-course-title">
+							Networking
+						</p>
+						<a href="#/Kanbas/Courses/1235/Home"> Go </a>
+					</div>
+				</div>
+				<div className="wd-dashboard-course">
+					<img src="/images/1236tn.jpg" width={200} />
+					<div>
+						<a className="wd-dashboard-course-link"
+							href="#/Kanbas/Courses/1236/Home">
+							CS1236 Game Development
+						</a>
+						<p className="wd-dashboard-course-title">
+							Game Development
+						</p>
+						<a href="#/Kanbas/Courses/1236/Home"> Go </a>
+					</div>
+				</div>
+				<div className="wd-dashboard-course">
+					<img src="/images/1237tn.jpg" width={200} />
+					<div>
+						<a className="wd-dashboard-course-link"
+							href="#/Kanbas/Courses/1237/Home">
+							CS1237 Cybersecurity
+						</a>
+						<p className="wd-dashboard-course-title">
+							Cybersecurity
+						</p>
+						<a href="#/Kanbas/Courses/1237/Home"> Go </a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);}

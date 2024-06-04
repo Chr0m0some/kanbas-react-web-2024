b91085d9dcfe735486b8c7c5a21127ee028909a1
Chris Lee <lee.christopher@northeastern.edu>
2024-06-04 15:22:03 -0500

    Finished module controls

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,37 +1,48 @@
+import ModulesControls from "./ModulesControls";
+
 export default function Modules() {
-    return (
-      <div>
-        <button>Collapse All</button>
-        <button>View Progress</button>
-        <select id="wd-publish">
-          <option value="SOME">Publish Some</option>
-          <option value="NONE">Publish None</option>
-          <option selected value="ALL">
-              Publish All</option>
-        </select>
-        <button>+ Module</button>
-        <ul id="wd-modules">
-          <li className="wd-module">
-            <div className="wd-title">Week 1</div>
-            <ul className="wd-lessons">
-              <li className="wd-lesson">
-                <span className="wd-title">LEARNING OBJECTIVES</span>
-                <ul className="wd-content">
-                  <li className="wd-content-item">Introduction to the course</li>
-                  <li className="wd-content-item">Learn what is Web Development</li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-          <li className="wd-module">
-            <div className="wd-title">Week 2</div>
-            <ul className="wd-lessons">
-              <li className="wd-lesson">
-                <span className="wd-title">LEARNING OBJECTIVES</span>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-  );}
-  
+  return (
+    <div id="wd-modules">
+      <ModulesControls /><br /><br /><br /><br />
+      <ul id="wd-modules" className="list-group rounded-0">
+        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title wd-title p-3 ps-2 bg-secondary">Week 1</div>
+          <ul className="wd-lessons list-group rounded-0">
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              LEARNING OBJECTIVES
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              Introduction to the course
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              Learn what is Web Development
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              LESSON 1
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              LESSON 2
+            </li>
+          </ul>
+        </li>
+        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title wd-title p-3 ps-2 bg-secondary">Week 2</div>
+          <ul className="wd-lessons list-group rounded-0">
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              LEARNING OBJECTIVES
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              Introduction to the course
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              LESSON 1
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              LESSON 2
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+}

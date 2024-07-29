e8a2f384635f2c38e89671ff475ce7f67cdc83b9
Chris <lee.christopher@northeastern.edu>
2024-07-29 14:52:30 -0500

    added ability to change roles and email

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,31 +1,31 @@
 import "./index.css";
 import { Link, useParams, useLocation } from "react-router-dom";
 export default function CoursesNavigation() {
-	const { pathname } = useLocation();
-	const { cid } = useParams();
-	const links = [
-		"Home",
-		"Modules",
-		"Piazza",
-		"Zoom",
-		"Assignments",
-		"Quizzes",
-		"Grades"
-	];
-	return (
-		<div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
-			{
-				links.map((link) => (
-					<Link
-						key={`/Kanbas/Courses/${cid}/${link}`}
-						to={`/Kanbas/Courses/${cid}/${link}`}
-						className={`list-group-item border-0 ${pathname.includes(link) ? "active" : "text-danger"
-							}`}
-					>
-						{link}
-					</Link>
-				))
-			}
-		</div>
-	);
+  const { pathname } = useLocation();
+  const { cid } = useParams();
+  const links = [
+    "Home",
+    "Modules",
+    "Piazza",
+    "Zoom",
+    "Assignments",
+    "Quizzes",
+    "Grades",
+    "People",
+  ];
+  return (
+    <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
+      {links.map((link) => (
+        <Link
+          key={`/Kanbas/Courses/${cid}/${link}`}
+          to={`/Kanbas/Courses/${cid}/${link}`}
+          className={`list-group-item border-0 ${
+            pathname.includes(link) ? "active" : "text-danger"
+          }`}
+        >
+          {link}
+        </Link>
+      ))}
+    </div>
+  );
 }
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -6,6 +6,7 @@ import CoursesNavigation from "./Navigation";
 import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
 import { FaAlignJustify } from "react-icons/fa6";
 import Grades from "./Grades/Grades";
+import PeopleTable from "./People/table";
 
 export default function Courses({ courses }: { courses: any[]; }) {
   const { cid } = useParams();
@@ -30,6 +31,8 @@ export default function Courses({ courses }: { courses: any[]; }) {
             <Route path="Assignments" element={<Assignments />} />
             <Route path="Assignments/:id" element={<AssignmentEditor />} />
             <Route path="Grades" element={<Grades />} />
+            <Route path="People" element={<PeopleTable />} />
+            <Route path="People/:uid" element={<PeopleTable />} />
           </Routes>
         </div>
       </div>

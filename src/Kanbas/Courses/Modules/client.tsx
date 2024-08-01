0e6aadbcf08ee20f9dc86f61594ce99e75784e5b
Chris <lee.christopher@northeastern.edu>
2024-08-01 10:41:09 -0500

    adjusted the API url in the modules client

--- src/Kanbas/Courses/Modules/client.tsx
+++ src/Kanbas/Courses/Modules/client.tsx
@@ -1,18 +1,14 @@
 import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
-const COURSES_API = `${REMOTE_SERVER}/api/courses`;
+const MODULES_API = `${REMOTE_SERVER}/api/modules`;
 export const findModulesForCourse = async (courseId: string) => {
-  const response = await axios.get(`${COURSES_API}/${courseId}/modules`);
+  const response = await axios.get(`${MODULES_API}/${courseId}`);
   return response.data;
 };
 export const createModule = async (courseId: string, module: any) => {
-  const response = await axios.post(
-    `${COURSES_API}/${courseId}/modules`,
-    module
-  );
+  const response = await axios.post(`${MODULES_API}/${courseId}`, module);
   return response.data;
 };
-const MODULES_API = `${REMOTE_SERVER}/api/modules`;
 export const deleteModule = async (moduleId: string) => {
   const response = await axios.delete(`${MODULES_API}/${moduleId}`);
   return response.data;
